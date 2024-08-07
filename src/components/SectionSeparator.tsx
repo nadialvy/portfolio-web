8887ce9e2d6c8c0a265982d07e5e2e61df3f49db
nadialvy <5026231007@student.its.ac.id>
2024-08-07 16:40:56 +0700

    fix: section separator component

--- src/components/SectionSeparator.tsx
+++ src/components/SectionSeparator.tsx
@@ -13,21 +13,21 @@ export default function SectionSeparator({numberFirst, number, firstTitle, secon
   return (
     <div
       className={clsxm(
-        "px-6 flex flex-col justify-start items-stretch w-full max-w-[1015px] h-[144px]",
-        "lg:items-end lg:px-0  lg:justify-between",
+        "px-6 flex flex-col justify-start items-start w-full max-w-[1015px] h-[200px]",
+        "lg:items-end lg:px-0 lg:justify-between",
         numberFirst ? "lg:flex-row" : "lg:flex-row-reverse"
       )}
     >
       <Typography
         font="playfairdisplay"
-        className="text-[180px] lg:text-[200px] lg:mt-12 font-extrabold self-stretch"
+        className="text-[180px] md:text-[180px] lg:text-[200px] font-extrabold leading-none lg:self-stretch"
       >
         {number}
       </Typography>
       <Typography
         as="h2"
         font="montserrat"
-        className="font-bold text-[28px] lg:text-[32px] mt-4 leading-none "
+        className="font-bold text-[28px] md:text-[28px] lg:text-[32px] mt-4 leading-none"
       >
         {firstTitle} <br /> {secondTitle}
       </Typography>
