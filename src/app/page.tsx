6cac6fbd88b08bd0010314fa1c07091bc4e7f65f
nadialvy <5026231007@student.its.ac.id>
2024-08-08 12:08:29 +0700

    feat: gallery image on project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -126,7 +126,7 @@ export default function Home() {
           <LineSeparator className="border-[0.5px] border-blackstone-200" />
         </div>
         <div className="px-6 lg:px-0 w-full py-12 flex space-x-12 overflow-x-auto container-snap lg:hidden">
-          <div className="border-blackstone-500 border-[0.3px] flex flex-col items-center justify-center w-3/4">
+          <div className="border-blackstone-500 border-[0.3px] flex flex-col items-center w-3/4">
             <Typography
               className="font-bold py-2"
               as="p"
@@ -170,55 +170,25 @@ export default function Home() {
             </div>
             <LineSeparator className="border-[0.5px] border-blackstone-200" />
 
-            <HorizontalScrollCard />
-          </div>
-          <div className=" border-blackstone-500 border-[0.3px] flex flex-col items-center justify-center w-3/4">
-            <Typography
-              className="font-bold py-2"
-              as="p"
-              variant="p"
-              font="montserrat"
-            >
-              PROJECT #1
-            </Typography>
-            <LineSeparator className="border-[0.5px] border-blackstone-200" />
-            <div className="flex flex-col items-center justify-center py-4">
-              <Typography
-                as="p"
-                variant="p"
-                className="font-bold"
-                font="montserrat"
-              >
-                INILHOITS! 2024
-              </Typography>
-              <Typography as="p" variant="p">
-                Staff Frontend
-              </Typography>
-            </div>
-            <LineSeparator className="border-[0.5px] border-blackstone-200" />
-            <div className="flex flex-col items-center justify-center p-4 text-balance">
-              <Typography className="text-center text-balance">
-                AAA Lorem ipsum dolor sit a Lorem ipsum Lorem dolor sit a ipsum
-                Lorem ipsum Lorem ipsum Lorem ipsum dolor sit a Lorem ipsum
-              </Typography>
-              <div className="flex items-center space-x-2 justify-center mt-2 group">
-                <Typography className="underline group-hover:cursor-pointer">
-                  See the web
-                </Typography>
-                <NextImage
-                  src="/icon/arrow.png"
-                  width={24}
-                  height={22}
-                  alt="arrow"
-                  className="group-hover:cursor-pointer group-hover:rotate-45 duration-300 transition-all w-4"
-                />
+            <div className="p-4">
+              <div className="w-80 flex flex-col space-y-2 h-full">
+                <div className="flex justify-between space-x-2 items-start">
+                  <div className="w-1/2 flex flex-col space-y-2">
+                    <div className="w-full bg-gray-200 h-40">pict1</div>
+                    <div className="w-full bg-gray-200 h-20">pict2</div>
+                    <div className="w-full bg-gray-200 h-20">pict3</div>
+                  </div>
+                  <div className="w-1/2 space-y-2 flex flex-col justify-between items-end">
+                    <div className="w-full bg-gray-200 h-20">pict4</div>
+                    <div className="w-full bg-gray-200 h-[248px]">pict5</div>
+                  </div>
+                </div>
+                <div className="w-full bg-gray-200 h-20">pict6</div>
               </div>
             </div>
-            <LineSeparator className="border-[0.5px] border-blackstone-200" />
-
-            <HorizontalScrollCard />
           </div>
-          <div className=" border-blackstone-500 border-[0.3px] flex flex-col items-center justify-center w-3/4">
+
+          <div className="border-blackstone-500 border-[0.3px] flex flex-col items-center w-3/4">
             <Typography
               className="font-bold py-2"
               as="p"
@@ -262,9 +232,25 @@ export default function Home() {
             </div>
             <LineSeparator className="border-[0.5px] border-blackstone-200" />
 
-            <HorizontalScrollCard />
+            <div className="p-4">
+              <div className="w-80 flex flex-col space-y-2 h-full">
+                <div className="flex justify-between space-x-2 items-start">
+                  <div className="w-1/2 flex flex-col space-y-2">
+                    <div className="w-full bg-gray-200 h-40">pict1</div>
+                    <div className="w-full bg-gray-200 h-20">pict2</div>
+                    <div className="w-full bg-gray-200 h-20">pict3</div>
+                  </div>
+                  <div className="w-1/2 space-y-2 flex flex-col justify-between items-end">
+                    <div className="w-full bg-gray-200 h-20">pict4</div>
+                    <div className="w-full bg-gray-200 h-[248px]">pict5</div>
+                  </div>
+                </div>
+                <div className="w-full bg-gray-200 h-20">pict6</div>
+              </div>
+            </div>
           </div>
-          <div className=" border-blackstone-500 border-[0.3px] flex flex-col items-center justify-center w-3/4">
+
+          <div className="border-blackstone-500 border-[0.3px] flex flex-col items-center w-3/4">
             <Typography
               className="font-bold py-2"
               as="p"
@@ -308,53 +294,22 @@ export default function Home() {
             </div>
             <LineSeparator className="border-[0.5px] border-blackstone-200" />
 
-            <HorizontalScrollCard />
-          </div>
-          <div className=" border-blackstone-500 border-[0.3px] flex flex-col items-center justify-center w-3/4">
-            <Typography
-              className="font-bold py-2"
-              as="p"
-              variant="p"
-              font="montserrat"
-            >
-              PROJECT #1
-            </Typography>
-            <LineSeparator className="border-[0.5px] border-blackstone-200" />
-            <div className="flex flex-col items-center justify-center py-4">
-              <Typography
-                as="p"
-                variant="p"
-                className="font-bold"
-                font="montserrat"
-              >
-                INILHOITS! 2024
-              </Typography>
-              <Typography as="p" variant="p">
-                Staff Frontend
-              </Typography>
-            </div>
-            <LineSeparator className="border-[0.5px] border-blackstone-200" />
-            <div className="flex flex-col items-center justify-center p-4 text-balance">
-              <Typography className="text-center text-balance">
-                Lorem ipsum dolor sit a Lorem ipsum Lorem dolor sit a ipsum
-                Lorem ipsum Lorem ipsum Lorem ipsum dolor sit a Lorem ipsum
-              </Typography>
-              <div className="flex items-center space-x-2 justify-center mt-2 group">
-                <Typography className="underline group-hover:cursor-pointer">
-                  See the web
-                </Typography>
-                <NextImage
-                  src="/icon/arrow.png"
-                  width={24}
-                  height={22}
-                  alt="arrow"
-                  className="group-hover:cursor-pointer group-hover:rotate-45 duration-300 transition-all w-4"
-                />
+            <div className="p-4">
+              <div className="w-80 flex flex-col space-y-2 h-full">
+                <div className="flex justify-between space-x-2 items-start">
+                  <div className="w-1/2 flex flex-col space-y-2">
+                    <div className="w-full bg-gray-200 h-40">pict1</div>
+                    <div className="w-full bg-gray-200 h-20">pict2</div>
+                    <div className="w-full bg-gray-200 h-20">pict3</div>
+                  </div>
+                  <div className="w-1/2 space-y-2 flex flex-col justify-between items-end">
+                    <div className="w-full bg-gray-200 h-20">pict4</div>
+                    <div className="w-full bg-gray-200 h-[248px]">pict5</div>
+                  </div>
+                </div>
+                <div className="w-full bg-gray-200 h-20">pict6</div>
               </div>
             </div>
-            <LineSeparator className="border-[0.5px] border-blackstone-200" />
-
-            <HorizontalScrollCard />
           </div>
         </div>
         <div className="hidden lg:block">
